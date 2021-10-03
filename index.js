da2e95dc01450d1cf0fc9a06462adcc54272c632
jmaclan <josh.aclan@gmail.com>
2021-10-04 07:45:10 +0800

    Add files

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// my_list = [[1,2,3,4],
+//              [1,4,5,6],
+//              [1,4,5,6]]
+             
+// for x in zip(*my_list):
+//     print(x)
+
+// col_totals = [ sum(x) for x in zip(*my_list) ]
+// print(col_totals)
